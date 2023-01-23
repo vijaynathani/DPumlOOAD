8ea5dbc505551210a0bca8603a39f9a49f107100
Vijay Nathani <vijay_nathani@yahoo.com>
2023-01-23 17:32:19 +0530

    All code of 20-Jan in one place

--- VSapp/Jan20.cpp
+++ VSapp/Jan20.cpp
@@ -4,128 +4,155 @@
 #include <map>
 #include <set>
 using namespace std;
+namespace jan20 {
+	class Party {
+	public:
+		string location;
+	};
+	class Person1 : public Party {};
+	class Organization : public Party {
+	public:
+		vector<Party*> children;
+	};
 
-class Party {
-public:
-	string location;
-};
-class Person1 : public Party {};
-class Organization : public Party {
-public:
-	vector<Party*> children;
-};
-
-class Author {
-public:
-	string name;
-	int age;
-};
-class Computer {
-public:
-	string name;
-	int memory;
-	Author* usedBy;
-};
-
-
-class Collection {
-public:
-	virtual bool equals(void* other) = 0;
-	virtual void add(void* item) = 0;
-};
-class List : public Collection {
-public:
-	virtual void* get() = 0;
-};
-class AbstractList : public List {
-public:
-	bool equals(void* other) override { return false; }
-	void add(void* item) override {}
-};
-class ArrayList : public AbstractList {
-public:
-	void* get() override { return nullptr; }
-	void add(void* item) override {}
-};
+	class Author {
+	public:
+		string name;
+		int age=0;
+	};
+	class Computer {
+	public:
+		string name;
+		int memory = 0;
+		Author* usedBy = nullptr;
+	};
 
 
-
-class EmailSystem {
-public:
-	virtual void send(string message) = 0;
-};
-class SMTPEmailSystem : public EmailSystem {
-public:
-	void send(string message) override {}
-
-};
-
-
-class Employee {};
-class Customer {
-	string name, address;
-public:
-	virtual string getCreditRating() { return ""; }
-};
-class CorporateCustomer : public Customer {
-	string contactName, creditRating;
-	double creditLimit;
-	Employee* salesRep;
-public:
-	string billForMonth(int month) { return ""; }
-	void remind() {}
-};
-class PersonalCustomer : public Customer {
-	string creditCardNumber;
-public:
-	string getCreditRating() override { return "poor"; }
-
-};
-
-class Person;
-class Car {
-	Person* owner;
-};
-class Person {
-	vector<Car*> cars;
-};
+	class Collection {
+	public:
+		virtual bool equals(void* other) = 0;
+		virtual void add(void* item) = 0;
+	};
+	class List : public Collection {
+	public:
+		virtual void* get() = 0;
+	};
+	class AbstractList : public List {
+	public:
+		bool equals(void* other) override { return false; }
+		void add(void* item) override {}
+	};
+	class ArrayList : public AbstractList {
+	public:
+		void* get() override { return nullptr; }
+		void add(void* item) override {}
+	};
 
 
-class Boolean {};
-class Date {};
-class Money {};
-class Product {};
-class OrderLine {
-	int quantity;
-	Money price;
-	Product* p;
-};
-class Order {
-	Boolean isPrePaid; 
-	Date* dateReceived;
-	string number;
-	Money price;
-	vector<OrderLine*> lineItems;
-	Customer *c;
-public:
-	void dispatch() {}
-	void close() {}
-};
 
-class Order1 {
-	//map <Product&, OrderLine*> lineItems;
-	//Above line is commented because map comparison functions are absent.
-};
+	class EmailSystem {
+	public:
+		virtual void send(string message) = 0;
+	};
+	class SMTPEmailSystem : public EmailSystem {
+	public:
+		void send(string message) override {}
 
-class Meeting {};
-class Attendance {
-	Person* p;
-	Meeting* m;
-	int attentiveness;
-};
-class Company {};
-class Employment {
-	Date start, end;
-	Person* employee;
-	Company* c;
-};
+	};
+
+
+	class Employee {};
+	class Customer {
+		string name, address;
+	public:
+		virtual string getCreditRating() { return ""; }
+	};
+	class CorporateCustomer : public Customer {
+		string contactName, creditRating;
+		double creditLimit;
+		Employee* salesRep;
+	public:
+		string billForMonth(int month) { return ""; }
+		void remind() {}
+	};
+	class PersonalCustomer : public Customer {
+		string creditCardNumber;
+	public:
+		string getCreditRating() override { return "poor"; }
+
+	};
+
+	class Person;
+	class Car {
+		Person* owner;
+	};
+	class Person {
+		vector<Car*> cars;
+	};
+
+
+	class Boolean {};
+	class Date {};
+	class Money {};
+	class Product {};
+	class OrderLine {
+		int quantity;
+		Money price;
+		Product* p;
+	};
+	class Order {
+		Boolean isPrePaid;
+		Date* dateReceived;
+		string number;
+		Money price;
+		vector<OrderLine*> lineItems;
+		Customer* c;
+	public:
+		void dispatch() {}
+		void close() {}
+	};
+
+	class Order1 {
+		//map <Product&, OrderLine*> lineItems;
+		//Above line is commented because map comparison functions are absent.
+	};
+
+	class Meeting {};
+	class Attendance {
+		Person* p;
+		Meeting* m;
+		int attentiveness;
+	};
+	class Company {};
+	class Employment {
+		Date start, end;
+		Person* employee;
+		Company* c;
+	};
+}
+using namespace jan20;
+void jan20main() {
+	Person1 Don, John;
+	Don.location = "Champaign";
+	John.location = "Champaign";
+	Organization engineering, tools, apps;
+	tools.location = "Chicago";
+	tools.children.push_back(&Don);
+	tools.children.push_back(&John);
+	apps.location = "saba";
+	engineering.location = "Boston";
+	engineering.children.push_back(&tools);
+	engineering.children.push_back(&apps);
+
+
+
+	Author bob;
+	bob.name = "Bob J";
+	bob.age = 32;
+	Computer bobPC;
+	bobPC.name = "Dell 466";
+	bobPC.memory = 64;
+	bobPC.usedBy = &bob;
+
+}
+
