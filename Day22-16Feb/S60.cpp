7ed2d5785a445d1206cb2600059a08e2ef346da0
Vijay Nathani <vijay_nathani@yahoo.com>
2023-02-16 18:57:59 +0530

    S61

--- Day22-16Feb/S60.cpp
+++ Day22-16Feb/S60.cpp
@@ -3,62 +3,36 @@
 #include <iostream>
 #include <stdexcept>
 using namespace std;
-/*
- * The goal is to classify numbers. A number is perfect, if the sum 
- * of its factors equals the number e.g. 6 is perfect. 
- * The factors are 1, 2, 3. 1 + 2 + 3 equals 6. 
- * A number is abundant, if the sum of its factors is greater than
- * the number e.g. 12 is abundant. The factors are 1, 2, 3, 4 and 6. 
- * 1+2+3+4+6 is greater than 12.
- * A number is deficient, if the sum of its factors is less
- * than the number e.g. 10 is deficient. The factors are 1, 2, 5. 
- * 1+2+5 is less than 10.
- */
 class NumberClassifier {
-    set<int> _factors;
-    int _number;
-    bool isFactor(int factor) {
-        return (_number % factor) == 0;
+    static bool isFactor(int number, int potential_factor) {
+        return (number % potential_factor) == 0;
     }
-    void calculateFactors() {
-        for (int i = 2; i <= sqrt(_number); i++)
-            if (isFactor(i))
-                addFactor(i);
+    static void add_factors(set<int> *factors, int number) {
+		factors->insert(1);
+        for (int i = 2; i <= sqrt(number); i++)
+            if (isFactor(number,i)) {
+                factors->insert(i);
+                factors->insert(number/i);
+			}
     }
-    void addFactor(int factor) {
-        _factors.insert(factor);
-        _factors.insert(_number / factor);
-    }
-    int sumOfFactors() {
-        calculateFactors();
+    static int sum_of_factors(int number) {
+		set<int> factors;
+		add_factors(&factors, number);
         int sum = 0;
-		for (set<int>::const_iterator p = _factors.begin();
-						p != _factors.end();
+		for (set<int>::const_iterator p = factors.begin();
+						p != factors.end();
 						++p )
 			sum += *p;
         return sum;
     }
 	public: 
-	NumberClassifier(int number) {
-        if (number < 1)
-            throw new runtime_error(
-                    "Can't classify negative numbers");
-        _number = number;
-        _factors.insert(1);
-    }
-    set<int> getFactors() {
-        return _factors;
-    }
-    bool isPerfect() {
-        return sumOfFactors() == _number;
-    }
-    bool isAbundant() {
-        return sumOfFactors() > _number;
-    }
-    bool isDeficient() {
-        return sumOfFactors() < _number;
-    }
     static bool isPerfect(int number) {
-        return NumberClassifier(number).isPerfect();
+        return sum_of_factors(number) == number;
+    }
+    static bool isAbundant(int number) {
+        return sum_of_factors(number) > number;
+    }
+    static bool isDeficient(int number) {
+        return sum_of_factors(number) < number;
     }
 };
---
+++ Day22-16Feb/S61.cpp
@@ -0,0 +1,82 @@
+#include <unordered_set>
+#include <algorithm>
+
+namespace tictactoe1{
+using namespace std;
+class Game {
+public:
+    static const int SIZE = 3, EMPTY = 0, CROSS = 1, CIRCLE = 2;
+
+    Game() {
+    	board = new int[SIZE * SIZE];
+        for (int r = 0; r < SIZE; ++r)
+            for (int c = 0; c < SIZE; ++c)
+                board[get_position(r,c)] =  EMPTY;
+    }
+    void put_x(int r, int c) {
+        board[get_position(r,c)] =  CROSS;
+    }
+    void put_o(int r, int c) {
+        board[get_position(r,c)] = CIRCLE;
+    }
+    bool is_empty(int r, int c) const {
+        return board[get_position(r,c)] == EMPTY;
+    }
+    bool is_over() const {
+    	return left_diagonal_filled_with_same_elements()
+    	                || right_diagonal_filled_with_same_elements()
+    	                || any_row_filled_with_same_elements()
+    	                || any_column_filled_with_same_elements()
+    	                || all_elements_of_board_are_occupied();
+    }
+    virtual ~Game() { delete[] board; }
+private:
+    Game(const Game&);
+    Game& operator=(const Game&);
+    int get_position(int row, int col) const { return row * SIZE + col; }
+
+    bool all_elements_of_board_are_occupied() const {
+        for (int r = 0; r < SIZE; ++r)
+            for (int c = 0; c < SIZE; ++c)
+            	if (board[get_position(r,c)] == EMPTY)
+            		return false;
+        return true;
+    }
+    bool any_row_filled_with_same_elements() const {
+        for (int r = 0; r < SIZE; ++r)
+            if (are_values_equal_but_not_zero(board + r * SIZE))
+                return true;
+        return false;
+    }
+    bool any_column_filled_with_same_elements() const {
+        for (int c = 0; c < SIZE; ++c) {
+        	int val[SIZE];
+        	for (int r=0; r<SIZE; ++r) val[r] = board[get_position(r,c)];
+            if (are_values_equal_but_not_zero(val)) return true;
+        }
+        return false;
+    }
+    bool right_diagonal_filled_with_same_elements() const {
+    	unordered_set<int> v;
+        for (int i = 0; i < SIZE; ++i)
+            v.insert(board[get_position(i, SIZE - i - 1)]);
+        return ensure_one_non_zero_value_present_in_set(v);
+    }
+    bool left_diagonal_filled_with_same_elements() const {
+    	unordered_set<int> v;
+        for (int i = 0; i < SIZE; ++i)
+            v.insert(board[get_position(i, i)]);
+        return ensure_one_non_zero_value_present_in_set(v);
+    }
+    static bool ensure_one_non_zero_value_present_in_set(const unordered_set<int> &values) {
+    	int val = Game::EMPTY;
+    	return find(values.cbegin(), values.cend(), val) == values.cend() && (values.size() == 1);
+    }
+    static bool are_values_equal_but_not_zero(const int mv[Game::SIZE]) {
+    	unordered_set<int> v;
+        for (int i = 0; i < Game::SIZE; ++i) v.insert(mv[i]);
+        return ensure_one_non_zero_value_present_in_set(v);
+    }
+    int *board = nullptr;
+};
+}
