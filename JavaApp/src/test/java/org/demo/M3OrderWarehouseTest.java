aa0fdf2f19f24eb4847de973d1949cf3146116d1
Vijay Nathani <vijay_nathani@yahoo.com>
2023-02-28 18:47:22 +0530

    Java first test

--- JavaApp/src/test/java/org/demo/M3OrderWarehouseTest.java
+++ JavaApp/src/test/java/org/demo/M3OrderWarehouseTest.java
@@ -3,4 +3,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class M3OrderWarehouseTest {
+    class MockWarehouse implements Warehouse {
+        int removeCalled, removeQuantity;
+        String removeProduct;
+        @Override
+        public boolean hasInventory(String product, int quantity) {
+            return true;
+        }
+        @Override
+        public void remove(String product, int quantity) {
+            removeCalled++;
+            removeProduct = product;
+            removeQuantity = quantity;
+        }
+    }
+    @Test public void InStockTesting() {
+        MockWarehouse mwh = new MockWarehouse();
+        String ipod = "ipod";
+        int quantity = 50;
+        Order order = new Order(ipod, quantity);
+        order.fill(mwh);
+        assertEquals(1, mwh.removeCalled);
+        assertEquals(ipod, mwh.removeProduct);
+        assertEquals(quantity, mwh.removeQuantity);
+    }
 }
