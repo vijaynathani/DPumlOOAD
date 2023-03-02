f2ed4777261b2467632245c1f93d996c9cd3b01d
Vijay Nathani <vijay_nathani@yahoo.com>
2023-03-02 18:52:52 +0530

    Update Mar02.cpp

--- VSapp/Mar02.cpp
+++ VSapp/Mar02.cpp
@@ -5,7 +5,42 @@
 #include <set>
 using namespace std;
 namespace mar02 {
+	class HasCoins;
+	class StampDispensor {
+		HasCoins* state = nullptr;
+	public:
+		StampDispensor();
+		void addCoins(int coins);
+		~StampDispensor() { delete state; }
+	};
+	class HasCoins {
+	public:
+		int coins = 0;
+		HasCoins(int coinsPresent) : coins{coinsPresent} {}
+		HasCoins* addCoins(int newCoinsAdded);
+	};
+	StampDispensor::StampDispensor() {
+		state = new HasCoins(0);
+	}
+	void StampDispensor::addCoins(int coins) {
+		state = state->addCoins(coins);
+	}
+	HasCoins* HasCoins::addCoins(int newCoinsAdded) {
+		const int STAMP_VALUE = 20;
+		coins += newCoinsAdded;
+		if (coins < STAMP_VALUE) return this;
+		cout << "Stamp dispensed" << endl;
+		coins -= STAMP_VALUE;
+		if (coins == 0) return this;
+		cout << "Refund for " << coins << " cents." <<endl;
+		coins = 0;
+		return this;
+	}
 }
 using namespace mar02;
 void mar02Main() {
+	StampDispensor sd;
+	sd.addCoins(5);
+	sd.addCoins(5);
+	sd.addCoins(20);
 }
